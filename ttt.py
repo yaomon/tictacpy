6bc32acc025a700c438a5795a50c0c4c22dfd82a
yaomon <yaomon18@yahoo.com>
2017-08-22 10:55:55 -0700

    Version 2
    Comments and game board size option

--- ttt.py
+++ ttt.py
@@ -1,9 +1,14 @@
+#Tic Tac Toe in Python
+#Author: Kyle Sun
+
 import numpy
 import sys
 
+#Draw row of lines to the given length
 def draw_row(size):
     print(' ---' * size)
-    
+
+#Draw box containing given symbol/character to the given length
 def draw_box(size, a):    
     for i in range(len(a)):
         if a[i] == 0:
@@ -17,6 +22,7 @@ def draw_box(size, a):
         print(p, end = "")
     print('|')
 
+#Finds if a given matrix has a winning row
 def find_win_row(a):    
     for j in range(len(a)):
         set_g = set(a[j])
@@ -24,6 +30,7 @@ def find_win_row(a):
             return a[j][0]
     return 0
 
+#Finds if a given matrix has a winning column
 def find_win_col(a):
     numpy.transpose(a)
     for j in range(len(a)):
@@ -32,14 +39,17 @@ def find_win_col(a):
             return a[j][0]
     return 0
 
+#Finds if a given matrix has a winning diagonal
 def find_win_dia(a):
     if a[1][1] != 0:
         if a[1][1] == a[0][0] == a[2][2]: 
-            return a[1][1]
+            return a[0][0]
         elif a[1][1] == a[0][2] == a[2][0]:
-            return a[1][1]			
+            return a[0][0]			
     return 0
 
+#Finds if a given matrix has a player who won or is full
+#Returns 0 on no win, 1/2 based on player won, or 3 for tie board
 def find_win(a):
     i = find_win_row(a)
     i = max(find_win_col(a), i)
@@ -50,24 +60,39 @@ def find_win(a):
         return 0
     else:
         return 3
-    
-while True:            
-    game = [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]]
 
-    #size = input('Size of board?')
-    #size = int(size)
-    size = 3
+#Main game loop
+while True:            
+    game = []
+    size = 0
+
+    #Get board size
+    while (size < 3):
+        size = input("Size of board? (3 or greater)")
+        size = int(size)
+        if (size < 3):
+            print("Size too small")
+
+    #Create game board
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(0)
+        game.append(row)    
+    
     p1 = 0
     p2 = 0
     end = False
+
+    #Draw board
     for i in range(len(game)):
         draw_row(size)
         draw_box(size, game[i])
     draw_row(size)
 
+    #Play loop
     while not end:
+        #Player 1 input
         flag = True
         while flag:
             try:
@@ -89,6 +114,7 @@ while True:
             draw_box(size, game[i])
         draw_row(size)
 
+        #Check for win
         w = find_win(game)
         if w != 0:
             if w == 3:
@@ -101,7 +127,8 @@ while True:
 
         if end:
             break
-        
+
+        #Player 2 input
         flag = True
         while flag:
             p2_move = input("P2 Move (row,column)")
@@ -123,6 +150,7 @@ while True:
             draw_box(size, game[i])
         draw_row(size)
 
+        #Check for win
         w = find_win(game)
         if w != 0:
             if w == 3:
@@ -133,6 +161,7 @@ while True:
                 end = True
                 p2 += 1
 
+    #Ask for another round
     print(p1, " - Player 1 | ", p2, " - Player 2")
     again = input("Play again? y/n")
     if again != "y":
